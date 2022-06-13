1017563e16dbf2a40faedf832e47e1f63b01c320
Abi Akindele <2366595+abbeymart@users.noreply.github.com>
2022-06-13 15:56:14 -0400

    added-more-tutorial-modules

--- learn/intro/kv_bucket/lib/kv/bucket.ex
+++ learn/intro/kv_bucket/lib/kv/bucket.ex
@@ -1,5 +1,13 @@
 defmodule KV.Bucket do
-  use Agent
+  use Agent, restart: :temporary
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    # Although we don't use the supervisor name below directly,
+    # it can be useful when debugging or introspecting the system.
+    KV.Supervisor.start_link(name: KV.Supervisor)
+  end
 
   @doc """
   Starts a new bucket.
